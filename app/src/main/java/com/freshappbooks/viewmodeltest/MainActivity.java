cb262e39a7dda294f332782aa540b8242b139eae
vjmarcus <vjmarcus1@gmail.com>
2020-04-14 17:28:46 +0300

    Add viewModel example without MutableLiveData

--- app/src/main/java/com/freshappbooks/viewmodeltest/MainActivity.java
+++ app/src/main/java/com/freshappbooks/viewmodeltest/MainActivity.java
@@ -1,29 +1,41 @@
 package com.freshappbooks.viewmodeltest;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
+    public static final String TAG = "MyApp";
     private TextView textView;
-    MainActivityViewModel viewModel = new ViewModelProvider(this).get(MainActivityViewModel.class);
+    MainActivityViewModel model;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Log.d(TAG, "onCreate: worked");
 
+        model = new ViewModelProvider(this).get(MainActivityViewModel.class);
         textView = findViewById(R.id.textview);
+        textView.setText(String.valueOf(model.currentValue()));
+
     }
 
-    public void increaseValue(View view){
-        textView.setText(viewModel.getIncreaseValue());
+    public void increaseValue(View view) {
+        Log.d(TAG, "increaseValue: worked");
+        textView.setText(String.valueOf(model.increaseValue()));
     }
+
     public void decreaseValue(View view) {
-        textView.setText(viewModel.getDecreaseValue());
+        Log.d(TAG, "decreaseValue: worked");
+        textView.setText(String.valueOf(model.decreaseValue()));
     }
 }
--- app/src/main/java/com/freshappbooks/viewmodeltest/MainActivityViewModel.java
+++ app/src/main/java/com/freshappbooks/viewmodeltest/MainActivityViewModel.java
@@ -1,15 +1,21 @@
 package com.freshappbooks.viewmodeltest;
 
+import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
 
 public class MainActivityViewModel extends ViewModel {
 
     private int count = 1;
 
-    public int getDecreaseValue() {
-        return count--;
+    public int increaseValue(){
+        return ++count;
     }
-    public int getIncreaseValue() {
-        return count++;
+    public int decreaseValue() {
+        return --count;
+    }
+    public int currentValue() {
+        return count;
     }
 }
+
+
